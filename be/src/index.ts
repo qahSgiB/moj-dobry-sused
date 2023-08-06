0d83496a8877be676fc94245e22f8f8a148380e2
qahSgiB <tomas.sumsala@gmail.com>
2023-08-06 22:26:17 +0200

    feat(be): set db name in env sample + style

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,11 +1,10 @@
-import express from 'express'
 import path from 'path';
 import fs from 'fs/promises';
 import { configDotenv } from 'dotenv';
+import express from 'express'
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
 import createHttpError from 'http-errors';
-
 
 import { offerRouter, userRouter } from './routes';
 
@@ -21,16 +20,17 @@ configDotenv();
 
 
 const app = express()
-// if request.content = json -> parse it as json
-app.use(express.json());
-// parse cookie header -> expose the cookie data as the property req.cookies
-app.use(cookieParser());
+
 // allow requests from frontend
 app.use(cors({
   origin: [process.env.FE_URL ?? 'http://localhost:5173'],
   methods: ['GET', 'POST'],
   credentials: true,
 }));
+// parse cookie header -> expose the cookie data as the property req.cookies
+app.use(cookieParser());
+// if request.content = json -> parse it as json
+app.use(express.json());
 
 // delegates incoming requests to specific routers
 app.use('/static', express.static(staticFolder));
@@ -60,8 +60,8 @@ if (process.env.BE_PORT !== undefined) {
 const serverPort = bePort === undefined ? 3000 : bePort;
 
 // set the server name from environment variable (default = localhost)
-const serverName = process.env.BE_HOSTNAME ?? 'localhost';
+const serverHostname = process.env.BE_HOSTNAME ?? 'localhost';
 
-app.listen(serverPort, serverName, () => {
-  console.log(`[moj-dobry-sused server] listening on 'http://${serverName}:${serverPort}'`);
+app.listen(serverPort, serverHostname, () => {
+  console.log(`[moj-dobry-sused server] listening on 'http://${serverHostname}:${serverPort}'`);
 })
--- be/src/utils/expressAsyncHandler.ts
+++ be/src/utils/expressAsyncHandler.ts
@@ -1,10 +1,14 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 
+
+
 // wraps async functions to be able to catch errors inside them
-const expressAsyncHandler = <P, ResBody, ReqBody, ReqQuery, Locals extends Record<string, any>>(handler: RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals>): RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals> => {
+const expressAsyncHandler = <P, ResBody, ReqBody, ReqQuery, Locals extends Record<string, unknown>>(handler: RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals>): RequestHandler<P, ResBody, ReqBody, ReqQuery, Locals> => {
   return (req: Request<P, ResBody, ReqBody, ReqQuery, Locals>, res: Response<ResBody, Locals>, next: NextFunction) => {
     Promise.resolve(handler(req, res, next)).catch((error) => next(error));
   }
 }
+
+
 
 export default expressAsyncHandler;
